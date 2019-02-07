ab335aceaf79c4185701eb0b46481e725f449f5c
it-door <tedchu4@gmail.com>
2019-02-07 04:59:28 -0800

    Added npm packages and began unit testing

---
+++ test/parser/parser-test.js
@@ -0,0 +1,26 @@
+/*
+ * Parser Tests
+ *
+ * Tests that the parser produces the expected abstract syntax tree for a
+ * variety of programs.
+ */
+
+const fs = require('fs');
+const assert = require('assert');
+const parse = require('../../syntax/parser');
+
+describe('The parser', () => {
+  fs.readdirSync(__dirname).forEach((name) => {
+    if (name.endsWith('.ded')) {
+      it(`produces the correct AST for ${name}`, (done) => {
+        fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, input) => {
+          const ast = parse(input);
+          fs.readFile(`${__dirname}/${name}.json`, 'utf-8', (_err, expected) => {
+            assert.deepEqual(ast, JSON.parse(expected));
+            done();
+          });
+        });
+      });
+    }
+  });
+});
