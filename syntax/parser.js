9e2c2f8de82028f93aa888eee8ab3d7d088adae5
ianlizzo <ianliz22@icloud.com>
2019-02-26 21:41:47 -0800

    ok

--- syntax/parser.js
+++ syntax/parser.js
@@ -14,30 +14,30 @@ const fs = require("fs");
 const ohm = require("ohm-js");
 const withIndentsAndDedents = require("./preparser.js");
 
-const Program = require('../ast/program');
-const VariableDeclaration = require('../ast/variable-declaration');
-const AssignmentStatement = require('../ast/assignment-statement');
-const BreakStatement = require('../ast/break-statement');
-const ReturnStatement = require('../ast/return-statement');
-const IfStatement = require('../ast/if-statement');
-const FromStatement = require('../ast/from-statement');
-const Case = require('../ast/case');
-const WhileStatement = require('../ast/while-statement');
-const CallStatement = require('../ast/call-statement');
-const FunctionDeclaration = require('../ast/function-declaration');
-const ListExpression = require('../ast/list-expression');
-const BinaryExpression = require('../ast/binary-expression');
-const UnaryExpression = require('../ast/unary-expression');
-const TernaryExpression = require('../ast/ternary-expression');
-const IdentifierExpression = require('../ast/identifier-expression');
-const SubscriptedExpression = require('../ast/subscripted-expression');
-const FunctionType = require('../ast/function-type');
-const Call = require('../ast/call');
-const Parameter = require('../ast/parameter');
-const Argument = require('../ast/argument');
-const BooleanLiteral = require('../ast/boolean-literal');
-const NumericLiteral = require('../ast/numeric-literal');
-const StringLiteral = require('../ast/string-literal');
+const Program = require("../ast/program");
+const VariableDeclaration = require("../ast/variable-declaration");
+const AssignmentStatement = require("../ast/assignment-statement");
+const BreakStatement = require("../ast/break-statement");
+const ReturnStatement = require("../ast/return-statement");
+const IfStatement = require("../ast/if-statement");
+const FromStatement = require("../ast/from-statement");
+const Case = require("../ast/case");
+const WhileStatement = require("../ast/while-statement");
+const CallStatement = require("../ast/call-statement");
+const FunctionDeclaration = require("../ast/function-declaration");
+const ListExpression = require("../ast/list-expression");
+const BinaryExpression = require("../ast/binary-expression");
+const UnaryExpression = require("../ast/unary-expression");
+const TernaryExpression = require("../ast/ternary-expression");
+const IdentifierExpression = require("../ast/identifier-expression");
+const SubscriptedExpression = require("../ast/subscripted-expression");
+const FunctionType = require("../ast/function-type");
+const Call = require("../ast/call");
+const Parameter = require("../ast/parameter");
+const Argument = require("../ast/argument");
+const BooleanLiteral = require("../ast/boolean-literal");
+const NumericLiteral = require("../ast/numeric-literal");
+const StringLiteral = require("../ast/string-literal");
 
 const grammar = ohm.grammar(fs.readFileSync("./syntax/casper.ohm"));
 
@@ -47,55 +47,120 @@ const grammar = ohm.grammar(fs.readFileSync("./syntax/casper.ohm"));
 }
 */
 /* eslint-disable no-unused-vars */
-const astGenerator = grammar.createSemantics().addOperation('ast', {
-  Program(_1, body, _2) { return new Program(body.ast()); },
-  Stmt_simple(statement, _) { return statement.ast(); },
-  Stmt_while(_, test, block) { return new WhileStatement(test.ast(), block.ast()); },
-  Stmt_from(_1, firstTest, _2, secondTest, _3, increments, Block ) { 
+const astGenerator = grammar.createSemantics().addOperation("ast", {
+  Program(_1, body, _2) {
+    return new Program(body.ast());
+  },
+  Stmt_simple(statement, _) {
+    return statement.ast();
+  },
+  Stmt_while(_, test, block) {
+    return new WhileStatement(test.ast(), block.ast());
+  },
+  Stmt_loop(_1, firstTest, _2, secondTest, _3, increments, Block) {
     const tests = [firstTest.ast(), secondTest.ast()];
-    return new FromStatement(tests, increments, Block.ast())},
+    return new FromStatement(tests, increments, Block.ast());
+  },
   Stmt_if(_1, firstTest, firstBlock, _2, moreTests, moreBlocks, _3, lastBlock) {
     const tests = [firstTest.ast(), ...moreTests.ast()];
     const bodies = [firstBlock.ast(), ...moreBlocks.ast()];
     const cases = tests.map((test, index) => new Case(test, bodies[index]));
     return new IfStatement(cases, unpack(lastBlock.ast()));
   },
-  Stmt_do(_1, firstTest, _2, secondTest, _3, thirdTest) {
+  Stmt_ternary(_1, firstTest, _2, secondTest, _3, thirdTest) {
     const tests = [firstTest.ast(), secondTest.ast(), thirdTest.ast()];
     return new TernaryExpression(tests);
   },
-  Stmt_def(_1, id, _2, params, _3, block) {
+  Stmt_function(_1, id, _2, params, _3, block) {
     return new FunctionDeclaration(id.ast(), params.ast(), block.ast());
   },
-  SimpleStmt_vardecl(_1, v, _2, e) { return new VariableDeclaration(v.ast(), e.ast()); },
-  SimpleStmt_assign(v, _, e) { return new AssignmentStatement(v.ast(), e.ast()); },
-  SimpleStmt_break(_) { return new BreakStatement(); },
-  SimpleStmt_return(_, e) { return new ReturnStatement(unpack(e.ast())); },
-  SimpleStmt_call(c) { return new CallStatement(c.ast()); },
-  Block_small(_1, statement, _2) { return [statement.ast()]; },
-  Block_large(_1, _2, _3, statements, _4) { return statements.ast(); },
-  Exp_or(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp_and(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp1_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp2_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp3_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp4_unary(op, operand) { return new UnaryExpression(op.ast(), operand.ast()); },
-  Exp5_unary(operand, op) { return new UnaryExpression(op.ast(), operand.ast()); },
-  Exp6_list(_1, expressions, _2) { return new ListExpression(expressions.ast()); },
-  Exp6_parens(_1, expression, _2) { return expression.ast(); },
-  FunctionType(_1, args, _2) { return FunctionType(args.ast()); },
-  Call(callee, _1, args, _2) { return new Call(callee.ast(), args.ast()); },
-  VarExp_subscripted(v, _1, e, _2) { return new SubscriptedExpression(v.ast(), e.ast()); },
-  VarExp_simple(id) { return new IdentifierExpression(id.ast()); },
-  Param(id, _, exp) { return new Parameter(id.ast(), unpack(exp.ast())); },
-  Arg(id, _, exp) { return new Argument(unpack(id.ast()), exp.ast()); },
-  NonemptyListOf(first, _, rest) { return [first.ast(), ...rest.ast()]; },
-  EmptyListOf() { return []; },
-  boollit(_) { return new BooleanLiteral(!!this.sourceString); },
-  numlit(_1, _2, _3, _4, _5, _6) { return new NumericLiteral(+this.sourceString); },
-  strlit(_1, chars, _6) { return new StringLiteral(this.sourceString); },
-  id(_1, _2) { return this.sourceString; },
-  _terminal() { return this.sourceString; },
+  SingleStmt_vardecl(_1, v, _2, e) {
+    return new VariableDeclaration(v.ast(), e.ast());
+  },
+  SingleStmt_assign(v, _, e) {
+    return new AssignmentStatement(v.ast(), e.ast());
+  },
+  SingleStmt_break(_) {
+    return new BreakStatement();
+  },
+  SingleStmt_return(_, e) {
+    return new ReturnStatement(unpack(e.ast()));
+  },
+  SingleStmt_call(c) {
+    return new CallStatement(c.ast());
+  },
+  Block_small(_1, statement, _2) {
+    return [statement.ast()];
+  },
+  Block_large(_1, _2, _3, statements, _4) {
+    return statements.ast();
+  },
+  Exp_or(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp_and(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp1_binary(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp2_binary(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp3_binary(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp4_unary(op, operand) {
+    return new UnaryExpression(op.ast(), operand.ast());
+  },
+  Exp5_unary(operand, op) {
+    return new UnaryExpression(op.ast(), operand.ast());
+  },
+  Exp6_list(_1, expressions, _2) {
+    return new ListExpression(expressions.ast());
+  },
+  Exp6_parens(_1, expression, _2) {
+    return expression.ast();
+  },
+  FunctionType(_1, args, _2) {
+    return FunctionType(args.ast());
+  },
+  Call(callee, _1, args, _2) {
+    return new Call(callee.ast(), args.ast());
+  },
+  VarExp_subscripted(v, _1, e, _2) {
+    return new SubscriptedExpression(v.ast(), e.ast());
+  },
+  VarExp_simple(id) {
+    return new IdentifierExpression(id.ast());
+  },
+  Param(id, _, exp) {
+    return new Parameter(id.ast(), unpack(exp.ast()));
+  },
+  Arg(id, _, exp) {
+    return new Argument(unpack(id.ast()), exp.ast());
+  },
+  NonemptyListOf(first, _, rest) {
+    return [first.ast(), ...rest.ast()];
+  },
+  EmptyListOf() {
+    return [];
+  },
+  boollit(_) {
+    return new BooleanLiteral(!!this.sourceString);
+  },
+  numlit(_1, _2, _3, _4, _5, _6) {
+    return new NumericLiteral(+this.sourceString);
+  },
+  strlit(_1, chars, _6) {
+    return new StringLiteral(this.sourceString);
+  },
+  id(_1, _2) {
+    return this.sourceString;
+  },
+  _terminal() {
+    return this.sourceString;
+  },
 });
 /* eslint-enable no-unused-vars */
 
