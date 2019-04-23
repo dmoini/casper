f01b4c59d0b81d58cad90e01146ea3563a666d75
Donovan Moini <donovanmoini@gmail.com>
2019-04-23 16:15:16 -0700

    WIP

--- ast/list-expression.js
+++ ast/list-expression.js
@@ -1,13 +1,16 @@
-// const check = require("../semantics/check");
+const check = require("../semantics/check");
 
 module.exports = class ListExpression {
   constructor(members) {
     this.members = members;
   }
 
-  // TODO
   analyze(context) {
     this.members.forEach(m => m.analyze(context));
-    // figure out how to check type inference
+    // TODO: Make sure it compiles
+    // we do not have lookupType function
+    const t = context.lookupType(this.type); // <-- Does this work? we don't have a type assigned to the expression
+    // isn't expressionsHaveTheSameType correct?
+    this.values.forEach(v => check.expressionsHaveTheSameType(v, t));
   }
 };
