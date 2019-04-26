a4f9439ab3e088a85ba9961920b052058a7370cd
Donovan Moini <donovanmoini@gmail.com>
2019-04-26 16:23:40 -0700

    Updated ternary expression

--- ast/ternary-expression.js
+++ ast/ternary-expression.js
@@ -1,3 +1,5 @@
+const check = require("../semantics/check");
+
 module.exports = class TernaryStatement {
   constructor(test, trueTest, falseTest) {
     Object.assign(this, { test, trueTest, falseTest });
@@ -5,6 +7,12 @@ module.exports = class TernaryStatement {
 
   // TODO: make ternary expressions work
   analyze(context) {
-    this.exp.analyze(context);
+    // this.exp.analyze(context);
+    this.test.analyze(context);
+    check.isBoolean(this.test);
+    this.trueTest.analyze(context);
+    this.trueTestType = this.trueTest.type;
+    this.falseTest.analyze(context);
+    this.falseTestType = this.falseTest.type;
   }
 };
