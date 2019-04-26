29cceb76323ee526529a01c3df84829e5b0648a1
Donovan Moini <donovanmoini@gmail.com>
2019-04-26 16:24:04 -0700

    Updated if statements to only accept booleans as tests

--- ast/if-statement.js
+++ ast/if-statement.js
@@ -1,10 +1,15 @@
+const check = require("../semantics/check");
+
 module.exports = class IfStatement {
   constructor(tests, consequents, alternate) {
     Object.assign(this, { tests, consequents, alternate });
   }
 
   analyze(context) {
-    this.tests.forEach(test => test.analyze(context));
+    this.tests.forEach((test) => {
+      test.analyze(context);
+      check.isBoolean(test);
+    });
     this.consequents.forEach((block) => {
       const blockContext = context.createChildContextForBlock();
       block.forEach(statement => statement.analyze(blockContext));
