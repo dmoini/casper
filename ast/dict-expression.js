6a441adac1d80e3725538d56df66630e2757474b
Donovan Moini <donovanmoini@gmail.com>
2019-04-30 15:18:58 -0700

    I hate eslint

--- ast/dict-expression.js
+++ ast/dict-expression.js
@@ -1,4 +1,4 @@
-const check = require("../semantics/check");
+// const check = require("../semantics/check");
 const DictType = require("./dict-type");
 
 module.exports = class DictExpression {
@@ -9,7 +9,7 @@ module.exports = class DictExpression {
   // eslint-disable-next-line no-unused-vars
   analyze(context) {
     // this.values.forEach(value => value.analyze(context));
-    this.exp.forEach(e => {
+    this.exp.forEach((e) => {
       e.key.analyze(context);
       e.value.analyze(context);
     });
@@ -19,10 +19,10 @@ module.exports = class DictExpression {
       this.type = new DictType(keyType, valueType);
       for (let i = 1; i < this.exp.length; i += 1) {
         if (
-          JSON.stringify(this.exp[i].key.type) !==
-            JSON.stringify(this.keyType) ||
-          JSON.stringify(this.exp[i].value.type) !==
-            JSON.stringify(this.valueType)
+          JSON.stringify(this.exp[i].key.type)
+            !== JSON.stringify(this.keyType)
+          || JSON.stringify(this.exp[i].value.type)
+            !== JSON.stringify(this.valueType)
         ) {
           throw new Error("Incompatible types within dictionary");
         }
