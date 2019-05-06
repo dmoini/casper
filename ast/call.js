859caaad5b734c67b686d36185ef14393293c95f
Donovan Moini <donovanmoini@gmail.com>
2019-05-06 16:45:40 -0700

    Removed comments

--- ast/call.js
+++ ast/call.js
@@ -6,7 +6,6 @@ module.exports = class CallExpression {
   analyze(context) {
     this.callee.analyze(context);
     this.args.forEach(arg => arg.analyze(context));
-    console.log(this.args);
     this.type = this.callee.ref.type;
     context.assertIsFunction(this.callee.ref);
     if (this.args.length !== this.callee.ref.params.length) {
@@ -14,7 +13,6 @@ module.exports = class CallExpression {
     }
     this.args.forEach((a, i) => {
       const paramType = this.callee.ref.params[i].type;
-      console.log("Param Type", this.callee.ref);
       if (a.expression.type !== paramType && paramType !== "void") {
         throw new Error("Argument and parameter types do not match");
       }
