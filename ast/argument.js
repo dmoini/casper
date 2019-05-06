6b38970ef11b8791d9118ede89b0c50bbd114183
Donovan Moini <donovanmoini@gmail.com>
2019-05-06 01:52:55 -0700

    More AST updates

--- ast/argument.js
+++ ast/argument.js
@@ -4,7 +4,7 @@ module.exports = class Argument {
   }
 
   analyze(context) {
-    console.log("ARG ANALYZE:", this.expression);
+    // console.log("ARG ANALYZE:", this.expression);
     this.expression.analyze(context);
   }
 };
