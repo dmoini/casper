9fc47361ad1ce1742b2a3cb21d571a571060c204
Maddie Louis <30633456+mlouis2@users.noreply.github.com>
2020-02-19 12:11:48 -0800

    remove some COMMENTS

--- semantics/check.js
+++ semantics/check.js
@@ -13,39 +13,7 @@ function doCheck(condition, message) {
 }
 
 module.exports = {
-  //   isPrimitiveType(type) {
-  //     doCheck(
-  //       type.constructor === NumType ||
-  //         type.constructor === StringType ||
-  //         type.constructor === BooleanType,
-  //       "Not a primitive type"
-  //     );
-  //   },
-
-  //   isListType(type) {
-  //     doCheck(type.constructor === ListType, "Not a list type");
-  //   },
-
-  //   isSetType(type) {
-  //     doCheck(type.constructor === SetType, "Not a set type");
-  //   },
-
-  //   isDictType(type) {
-  //     doCheck(type.constructor === DictType, "Not a dictionary type");
-  //   },
-
-  // isList(expression) {
-  //   doCheck(expression.type.constructor === ListType, "Not a list");
-  // },
-
-  //   isSet(expression) {
-  //     doCheck(expression.type.constructor === SetType, "Not a set");
-  //   },
-
-  // isDict(expression) {
-  //   doCheck(expression.type.constructor === DictType, "Not a dictionary");
-  // },
-
+  
   isListOrDict(expression) {
     doCheck(
       expression.type.constructor === ListType
@@ -61,20 +29,9 @@ module.exports = {
       || expression.constructor === DictType;
   },
 
-  // isListOrSet(expression) {
-  //   doCheck(
-  //     expression.type.constructor === ListType
-  //       || expression.type.constructor === SetType,
-  //     "Not list or set",
-  //   );
-  // },
-
   isNumber(exp) {
     doCheck(exp.type === NumType, "Not a number");
   },
-  //   isString(exp) {
-  //     doCheck(exp.type === StringType, "Not a string");
-  //   },
 
   isNumberOrString(exp) {
     doCheck(
@@ -86,10 +43,6 @@ module.exports = {
   isBoolean(exp) {
     doCheck(exp.type === BooleanType, "Not a boolean");
   },
-
-  //   isFunction(val) {
-  //     doCheck(val.constructor === Function, "Not a function");
-  //   },
 
   isAssignableTo(exp, type) {
     doCheck(
@@ -125,11 +78,4 @@ module.exports = {
     return e.left instanceof BooleanLiteral && e.right instanceof BooleanLiteral;
   },
 
-  //   legalArguments(args, params) {
-  //     doCheck(
-  //       args.length === params.length,
-  //       `Expected ${params.length} args in call, got ${args.length}`
-  //     );
-  //     args.forEach((arg, i) => this.isAssignableTo(arg, params[i].type));
-  //   },
 };
